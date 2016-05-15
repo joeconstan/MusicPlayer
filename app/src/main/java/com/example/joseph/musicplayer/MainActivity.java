5242db8c8eaf136860b0ebf13503e4058eb89c5e
fernandomontesdeoca <ferniemontesdeoca@gmail.com>
2016-05-15 16:02:37 -0700

    kokkj

--- app/src/main/java/com/example/joseph/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/joseph/musicplayer/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.joseph.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        changes
-                fuck github ^3
+        Button button = (Button)findViewById(R.id.Button);
+        final TextView textView = (TextView)findViewById(R.id.TextView);
+        final EditText editText = (EditText)findViewById(R.id.EditText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                textView.setText(text);
+
+            }
+        });
+
     }
 }
