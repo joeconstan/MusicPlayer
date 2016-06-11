2453c43c9483f217f344728b6ba82de0e7fbaaef
Joe <jconstan@bu.edu>
2016-06-11 12:15:30 -0700

    more functions, less spaghetti

---
+++ app/src/main/java/com/example/joseph/musicplayer/Song.java
@@ -0,0 +1,25 @@
+package com.example.joseph.musicplayer;
+
+
+public class Song
+{
+    private String title;
+    //private String artist;
+    //private String Album;
+    private String track;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTrack() {
+        return track;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setTrack(String track) {
+        this.track = track;
+    }
+}
