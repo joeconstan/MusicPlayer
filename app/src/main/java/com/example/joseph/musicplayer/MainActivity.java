0397d84e55a583314c44faced7eb522b086144ab
Joe <jconstan@bu.edu>
2016-06-11 14:43:38 -0700

    more functions, less spaghetti

--- app/src/main/java/com/example/joseph/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/joseph/musicplayer/MainActivity.java
@@ -38,11 +38,11 @@ import java.util.Vector;
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback{
 
 
-    public boolean queueSongs(Cursor cursor, Vector<String> songs){
+    public boolean queueSongs(Cursor cursor, Song songs){
         if (cursor.moveToFirst()) { //needs to check for first element to avoid nullptr exception
-            do {
-                songs.add(cursor.getString(1));
-            } while (cursor.moveToNext());
+            for (int i=0;i<30;i++){
+                songs[i].setTitle(cursor.getString(1));
+            } (cursor.moveToNext());
         }
 
 
@@ -69,13 +69,11 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
                 }
             }
 
-
-
         final ContentResolver resolver = getContentResolver();
         String[] projection = new String[]{BaseColumns._ID, MediaStore.MediaColumns.TITLE, MediaStore.Audio.Media.DATA};
-        //final Cursor cursor = resolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, MediaStore.Audio.Media.DATA, null, null);
         final Cursor cursor = resolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
-        Vector<String> songs = new Vector<>(0);
+        //Vector<String> songs = new Vector<>(0);
+        Song songs[] = new Song[30];
         queueSongs(cursor, songs);
         cursor.moveToFirst();
         ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, songs);
